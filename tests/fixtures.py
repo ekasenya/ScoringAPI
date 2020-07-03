a8b747afa916b27b423a3c81d20eeb01e2679179
katty87 <esenenkova@gmail.com>
2020-07-03 20:57:50 +0300

    Add field's tests

--- tests/fixtures.py
+++ tests/fixtures.py
@@ -10,14 +10,12 @@ def unavailable_store(request):
     mocked_redis.get.side_effect = ConnectionError
     mocked_redis.set.side_effect = ConnectionError
     with patch('scoring.ScoreStore.create_store', return_value=mocked_redis):
-        print('create unavailablestore')
         yield ScoreStore()
 
 
 @pytest.fixture(scope="module")
 def store(request):
     mocked_redis = Mock()
-    mocked_redis.get.return_value = 1
+    mocked_redis.get.return_value = None
     with patch('scoring.ScoreStore.create_store', return_value=mocked_redis):
-        print('create normal store')
         yield ScoreStore()
